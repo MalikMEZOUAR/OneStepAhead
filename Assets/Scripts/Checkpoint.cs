505494b39f4686eb68aa83a2aa3d437a88349e35
MalikMEZOUAR <b.m.mezouar@gmail.com>
2025-03-24 16:42:22 +0100

    Animation checkpoint

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+public Animator animator;
+    
     public BoxCollider2D bc2d;
     public Vector3Variable lastCheckpointPosition;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            animator.SetTrigger("Collision");
             PlayerSpawn playerSpawn = collision.GetComponent<PlayerSpawn>();
             if (playerSpawn != null)
             {
