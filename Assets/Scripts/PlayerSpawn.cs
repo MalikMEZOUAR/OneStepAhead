d2cdda7f7e7ccd1a50fcdb667174715031228c01
MalikMEZOUAR <b.m.mezouar@gmail.com>
2025-03-26 16:47:03 +0100

    SpawnPoint géré

--- Assets/Scripts/PlayerSpawn.cs
+++ Assets/Scripts/PlayerSpawn.cs
@@ -3,18 +3,31 @@ using UnityEngine;
 public class PlayerSpawn : MonoBehaviour
 {
     public Vector3Variable lastCheckpointPosition;
+    
     [Tooltip("Define where the player will spawn if there is an issue"), ReadOnlyInspector]
     public Vector3 currentSpawnPosition;
+
     [Tooltip("Define where the player started the level"), ReadOnlyInspector]
     public Vector3 initialSpawnPosition;
+
     private void Awake()
-    {     
-        if (lastCheckpointPosition.CurrentValue != null){
-            transform.position = lastCheckpointPosition.CurrentValue.Value;
-        }else{
-            lastCheckpointPosition.CurrentValue = transform.position;
+    {
+        GameObject spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint");
+
+        if (spawnPoint != null)
+        {
+            transform.position = spawnPoint.transform.position; // Déplace le joueur au spawn défini dans la scène
         }
-        currentSpawnPosition = gameObject.transform.position;
-        initialSpawnPosition = gameObject.transform.position;
+        else if (lastCheckpointPosition.CurrentValue != null)
+        {
+            transform.position = lastCheckpointPosition.CurrentValue.Value; // Utilise le dernier checkpoint
+        }
+        else
+        {
+            lastCheckpointPosition.CurrentValue = transform.position; // Sauvegarde la position de départ
+        }
+
+        currentSpawnPosition = transform.position;
+        initialSpawnPosition = transform.position;
     }
 }
